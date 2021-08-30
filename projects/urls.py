19d9b92bc5573a823c381faf028a7083eec7fa5b
El Mehdi JAMAI <jamai.elmehdi@gmail.com>
2021-08-30 11:52:12 +0200

    adding styles

--- projects/urls.py
+++ projects/urls.py
@@ -7,5 +7,9 @@ from .views import *
 urlpatterns = [
     path('', projects, name="projects"),
     path('project/<str:pk>/', project, name='project'),
+    path('create-project/', createProject, name = 'createProject'),
+    path('delete-project/<str:pk>/', deleteProject, name = 'deleteProject'),
+    #path('confirm-delete-project/<str:pk>/', confirmDeleteProject, name = 'confirmDeleteProject'),
+    path('update-project/<str:pk>/', updateProject, name = 'updateProject'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projectObjs = Project.objects.filter(status='active')
+    context = {'projects' : projectObjs}
+    return render(request, 'projects/projects.html',context)
 
 
 def project(request,pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = Project.objects.get(id=pk)
+    context = {'project' : projectObj}
+    return render(request, 'projects/single-project.html',context)
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form' : form}
+    return render(request, "projects/project_form.html",context)
+
+# def confirmDeleteProject(request,pk):
+#     projectObj = Project.objects.get(id=pk)
+#     context = {"project" : projectObj}
+#     return render(request, "projects/delete_template.html",context)
+
+# def deleteProject(request,pk):
+#     projectObj = Project.objects.get(id=pk)
+#     projectObj.status = 'desactivated'
+#     projectObj.save()
+#     return redirect('projects')
+
+def deleteProject(request,pk):
+     projectObj = Project.objects.get(id=pk)
+     context = {"project" : projectObj}
+     if request.method == "POST":
+        projectObj = Project.objects.get(id=pk)
+        projectObj.status = 'desactivated'
+        projectObj.save()
+        return redirect('projects')         
+     return render(request, "projects/delete_template.html",context)
+
+
+def updateProject(request,pk):
+    projectObj = Project.objects.get(id=pk)
+    form = ProjectForm(instance=projectObj)
+    if request.method == "POST":
+        form = ProjectForm(request.POST,instance=projectObj)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form' : form}
+    return render(request, "projects/project_form.html",context)
+
